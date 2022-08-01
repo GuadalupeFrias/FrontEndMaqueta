c1434535c203d9210bb2ff64099923fccd1d93ce
GuadalupeFrias <frias.guadalupe.hola@gmail.com>
2022-08-01 20:41:41 -0300

    cambios

--- scripts.js
+++ scripts.js
@@ -1,3 +1,27 @@
 
 $(".nombre").append("Guadalupe");
 $(".apellido").append("Frias");
+const contenido_acerca_de=document.getElementById("contenido_acerca_de");
+$("#edicion_formulario").hide();
+const edicionFormulario=document.getElementById("edicionFormulario");
+
+document.getElementById("boton_eliminar").onclick = eliminar;
+function eliminar() {
+   contenido_acerca_de.remove()
+  }
+
+document.getElementById("boton_edicion").onclick = edicion;
+function edicion() {
+  document.getElementById("edicion_formulario").innerText = edicionFormulario;
+  }
+
+
+
+/*
+$('#boton_edicion').click(function(){
+    $('#myModal').modal(('show'))
+});
+$('#myModal').on('shown.bs.modal', function(){
+    console.log("mostrando modal");
+});
+*/
